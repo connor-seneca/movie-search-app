70385a383b9f6adbcf95a26c776c6da99892e9e5
connor-seneca <cwsquires@myseneca.ca>
2025-07-02 14:53:35 -0400

    Added API key and finished display of movie titles/images when searched

--- src/App.js
+++ src/App.js
@@ -7,12 +7,29 @@ function App() {
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const API_KEY = '';
+  const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 
   const searchMovies = async () => {
-    if (!query) return;
+    if (!query) {
+      return;
+    }
     setLoading(true);
     setError('');
+    try {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`);
+      const data = await res.json();
+
+      if (data.Response === "True") {
+        setMovies(data.Search);
+      } else {
+        setError(data.Error);
+        setMovies([]);
+      }
+    } catch(err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -25,6 +42,18 @@ function App() {
         onChange={(e) => setQuery(e.target.value)}
       />
       <button onClick={searchMovies}>Search</button>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      <div className="movie-list">
+        {movies.map((movie => (
+          <div key={movie.imdbID} className="movie">
+            <h3>{movie.Title} ({movie.Year})</h3>
+            <img src={movie.Poster} alt={movie.Title}></img>
+          </div>
+        )))}
+      </div>
     </div>
   );
 }
